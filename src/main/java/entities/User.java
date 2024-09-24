f0da3ef4e5056ba1d8d2ae7d753da653ec15c292
sembekov <76952248+sembekov@users.noreply.github.com>
2024-09-24 21:45:21 +0500

    Add entity

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,23 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullName;
+    private String username;
+    private String email;
+    private String phoneNumber;
+}
