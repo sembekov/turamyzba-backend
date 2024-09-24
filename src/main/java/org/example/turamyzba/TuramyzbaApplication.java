6459af3b0b1b19441f31a34fefc797f716aca82a
sembekov <76952248+sembekov@users.noreply.github.com>
2024-09-24 19:39:00 +0500

    Initial commit

---
+++ src/main/java/org/example/turamyzba/TuramyzbaApplication.java
@@ -0,0 +1,13 @@
+package org.example.turamyzba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuramyzbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuramyzbaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/turamyzba/TuramyzbaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.turamyzba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuramyzbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
