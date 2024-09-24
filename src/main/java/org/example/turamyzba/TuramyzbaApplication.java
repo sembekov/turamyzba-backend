971f1a8cdfe1897ca2e25e1fae862d6cce465722
sembekov <76952248+sembekov@users.noreply.github.com>
2024-09-24 21:06:35 +0500

    Refactor the application

--- src/main/java/org/example/turamyzba/TuramyzbaApplication.java
+++ src/main/java/org/example/turamyzba/TuramyzbaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TuramyzbaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TuramyzbaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TuramyzbaApplication.class, args);
+	}
 
 }
